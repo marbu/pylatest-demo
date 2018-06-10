5bfb9b7a41bd0d8da6435a22e78033956a187386
Martin Bukatovic <martin.bukatovic@gmail.com>
2018-06-10 20:51:04 +0200

    enable pylatest sphinx plugin

--- conf.py
+++ conf.py
@@ -39,6 +39,7 @@ release = u''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'pylatest',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
